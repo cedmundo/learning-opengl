5aa4136fe80de84a67597b7ca37eef97409a5e06
Carlos Edmundo Martínez Mendoza <ced.mendoza@gmail.com>
2017-08-01 17:28:55 -0500

    Started materials lesson

---
+++ 10-Lighting-Materials/include/lmath/lmath.h
@@ -0,0 +1,13 @@
+#ifndef LMATH_H
+#define LMATH_H
+#include "vec2.h"
+#include "vec3.h"
+#include "vec4.h"
+#include "mat4.h"
+
+static inline float lmath_radians(float value) {
+    return value * 0.01745329252f;
+
+}
+
+#endif
---
+++ 10-Lighting-Materials/include/lmath/vec3.h
@@ -0,0 +1,123 @@
+#ifndef LMATH_VEC3_H
+#define LMATH_VEC3_H
+#include <math.h>
+#include "vec2.h"
+
+typedef union {
+    struct {
+        float x;
+        float y;
+        float z;
+    };
+    struct {
+        float yaw;
+        float pitch;
+        float roll;
+    };
+    float xy[2];
+    float xyz[3];
+} vec3;
+
+static const vec3 vec3_zero = {0.0f, 0.0f, 0.0f};
+static const vec3 vec3_up = {0.0f, 1.0f, 0.0f};
+static const vec3 vec3_one = {1.0f, 1.0f, 1.0f};
+
+static inline vec3 vec3_make(float x, float y, float z) {
+    vec3 r;
+    r.x = x;
+    r.y = y;
+    r.z = z;
+    return r;
+}
+
+static inline vec3 vec3_make1(vec2 v, float z) {
+    vec3 r;
+    r.x = v.x;
+    r.y = v.y;
+    r.z = z;
+    return r;
+}
+
+static inline vec3 vec3_add(const vec3 a, const vec3 b) {
+    vec3 r;
+    r.x = a.x + b.x;
+    r.y = a.y + b.y;
+    r.z = a.z + b.z;
+    return r;
+}
+
+static inline vec3 vec3_sub(const vec3 a, const vec3 b) {
+    vec3 r;
+    r.x = a.x - b.x;
+    r.y = a.y - b.y;
+    r.z = a.z - b.z;
+    return r;
+}
+
+static inline vec3 vec3_scale(const vec3 a, const float s) {
+    vec3 r;
+    r.x = a.x * s;
+    r.y = a.y * s;
+    r.z = a.z * s;
+    return r;
+}
+
+static inline float vec3_mul_inner(const vec3 a, const vec3 b) {
+    float r = 0.0f;
+    r += a.x * b.x;
+    r += a.y * b.y;
+    r += a.z * b.z;
+    return r;
+}
+
+static inline float vec3_sqr_len(const vec3 a) {
+    return vec3_mul_inner(a, a);
+}
+
+static inline float vec3_len(const vec3 a) {
+    return sqrtf(vec3_sqr_len(a));
+}
+
+static inline vec3 vec3_norm(const vec3 a) {
+    float k = 1.0f / vec3_len(a);
+    return vec3_scale(a, k);
+}
+
+static inline vec3 vec3_min(const vec3 a, const vec3 b) {
+    vec3 r;
+    r.x = a.x < b.x ? a.x : b.x;
+    r.y = a.y < b.y ? a.y : b.y;
+    r.z = a.z < b.z ? a.z : b.z;
+    return r;
+}
+
+static inline vec3 vec3_max(const vec3 a, const vec3 b) {
+    vec3 r;
+    r.x = a.x > b.x ? a.x : b.x;
+    r.y = a.y > b.y ? a.y : b.y;
+    r.z = a.z > b.z ? a.z : b.z;
+    return r;
+}
+
+static inline vec3 vec3_mul_cross(const vec3 a, const vec3 b) {
+    vec3 r;
+    r.x = a.y * b.z - a.z * b.y;
+    r.y = a.z * b.x - a.x * b.z;
+    r.z = a.x * b.y - a.y * b.x;
+    return r;
+}
+
+static inline vec3 vec3_reflect(const vec3 a, const vec3 b) {
+    float p = 2.0f * vec3_mul_inner(a, b);
+    vec3 r;
+    r.x = a.x - b.x * p;
+    r.y = a.y - b.y * p;
+    r.z = a.z - b.z * p;
+    return r;
+}
+
+static inline float vec3_dot(const vec3 a, const vec3 b) {
+    return a.x * b.x + a.y * b.y + a.z * b.z;
+}
+
+#endif
