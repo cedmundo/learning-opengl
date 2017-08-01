29edfab6e185257765bd639d381438d677ca5362
Carlos Edmundo Martínez Mendoza <ced.mendoza@gmail.com>
2017-08-01 17:17:06 -0500

    Major fix on transform.c (bad operation order)

--- 07-Camera/src/transform.c
+++ 07-Camera/src/transform.c
@@ -5,14 +5,13 @@ mat4 loglTransformGetModel(loglTransform *transform) {
     mat4 t, r, rx, ry, rz, s, m;
 
     s = mat4_make_scale(transform->sca);
-    t = mat4_make_translation(transform->pos);
-    m = mat4_mul(t, s);
-
     rx = mat4_make_rotation_x(transform->rot.x);
     ry = mat4_make_rotation_y(transform->rot.y);
     rz = mat4_make_rotation_z(transform->rot.z);
-    m = mat4_mul(m, rx);
+    m = mat4_mul(s, rx);
     m = mat4_mul(m, ry);
     m = mat4_mul(m, rz);
+    t = mat4_make_translation(transform->pos);
+    m = mat4_mul(m, t);
     return m;
 }
