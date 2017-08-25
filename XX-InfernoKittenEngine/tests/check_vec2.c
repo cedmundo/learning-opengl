88fbec94e4138923b7e478e452dfa468403c97c2
Carlos Edmundo Martínez Mendoza <ced.mendoza@gmail.com>
2017-08-25 18:30:50 -0500

    Completed most of the tests for vec2, missing some cases and norm is failing

--- XX-InfernoKittenEngine/tests/check_vec2.c
+++ XX-InfernoKittenEngine/tests/check_vec2.c
@@ -19,21 +19,18 @@ START_TEST(test_vec2_add)
     vec2 a = vec2_make(.5f, 5.f);
     vec2 b = vec2_make(.5f, 5.f);
     vec2 e = vec2_make(1.f, 10.f);
-
     vec2 c = vec2_add(a, b);
     ck_assert_vec2_eq(c, e);
 
     a = vec2_make(-.5f,  5.f);
     b = vec2_make( .5f, -5.f);
     e = vec2_make( 0.f,  0.f);
-
     c = vec2_add(a, b);
     ck_assert_vec2_eq(c, e);
 
     a = vec2_make(-.5f, -5.f);
     b = vec2_make(-.5f, -5.f);
     e = vec2_make(-1.f, -10.f);
-
     c = vec2_add(a, b);
     ck_assert_vec2_eq(c, e);
 }
@@ -44,16 +41,112 @@ START_TEST(test_vec2_sub)
     vec2 a = vec2_make(1.f, 10.f);
     vec2 b = vec2_make(.5f, 5.f);
     vec2 e = vec2_make(.5f, 5.f);
-
     vec2 c = vec2_sub(a, b);
     ck_assert_vec2_eq(c, e);
 
     a = vec2_make(-.5f,  5.f);
     b = vec2_make( .5f, -5.f);
     e = vec2_make(-1.f, 10.f);
-
     c = vec2_sub(a, b);
     ck_assert_vec2_eq(c, e);
+
+    a = vec2_make(-.5f, -5.f);
+    b = vec2_make(-.5f, -5.f);
+    e = vec2_make(0.f, 0.f);
+    c = vec2_sub(a, b);
+    ck_assert_vec2_eq(c, e);
+}
+END_TEST
+
+START_TEST(test_vec2_scale)
+{
+    float b = 3.f;
+    vec2 a = vec2_make(1.f, 10.f);
+    vec2 e = vec2_make(3.f, 30.f);
+
+    vec2 c = vec2_scale(a, b);
+    ck_assert_vec2_eq(c, e);
+
+    a = vec2_make(1.f, 10.f);
+    b = -3.f;
+    e = vec2_make(-3.f, -30.f);
+    c = vec2_scale(a, b);
+    ck_assert_vec2_eq(c, e);
+
+    a = vec2_make(-1.f, -10.f);
+    b = -3.f;
+    e = vec2_make(3.f, 30.f);
+    c = vec2_scale(a, b);
+    ck_assert_vec2_eq(c, e);
+}
+END_TEST
+
+START_TEST(test_vec2_mul_inner)
+{
+    vec2 a = vec2_make(2.f, 10.f);
+    vec2 b = vec2_make(3.f, 2.f);
+    float e = 26.f;
+    float c = vec2_mul_inner(a, b);
+    ck_assert(c == e);
+
+    // TODO: Negative cases
+}
+END_TEST
+
+START_TEST(test_vec2_sqr_len)
+{
+    vec2 a = vec2_make(2.f, 2.f);
+    float e = 8.f;
+    float c = vec2_sqr_len(a);
+    ck_assert(c == e);
+
+    // TODO: Negative cases
+}
+END_TEST
+
+START_TEST(test_vec2_len)
+{
+    vec2 a = vec2_make(2.f, 2.f);
+    float e = sqrtf(8.f);
+    float c = vec2_len(a);
+    ck_assert(c == e);
+
+    // TODO: Negative cases
+}
+END_TEST
+
+START_TEST(test_vec2_norm)
+{
+    vec2 a = vec2_make(1.f, 1.f);
+    vec2 e = vec2_make(1.f, 1.f);
+    vec2 c = vec2_norm(a);
+    ck_assert_vec2_eq(c, e);
+
+    // TODO: Negative cases
+}
+END_TEST
+
+START_TEST(test_vec2_min)
+{
+    vec2 a = vec2_make(3.f, 6.f);
+    vec2 b = vec2_make(2.f, 8.f);
+    vec2 e = vec2_make(2.f, 6.f);
+    vec2 c = vec2_min(a, b);
+    ck_assert_vec2_eq(c, e);
+
+    // TODO: Negative cases
+}
+END_TEST
+
+START_TEST(test_vec2_max)
+{
+    vec2 a = vec2_make(3.f, 6.f);
+    vec2 b = vec2_make(2.f, 8.f);
+    vec2 e = vec2_make(3.f, 8.f);
+    vec2 c = vec2_max(a, b);
+    ck_assert_vec2_eq(c, e);
+
+    // TODO: Negative cases
 }
 END_TEST
 
@@ -63,14 +156,25 @@ END_TEST
 Suite *vec2_suite(void)
 {
     Suite *s;
-    TCase *tc_core;
+    TCase *tc_arithm, *tc_access;
 
     s = suite_create("ike/vec2");
-    tc_core = tcase_create("core");
-    tcase_add_test(tc_core, test_vec2_access);
-    tcase_add_test(tc_core, test_vec2_add);
-    tcase_add_test(tc_core, test_vec2_sub);
-    suite_add_tcase(s, tc_core);
+
+    tc_access = tcase_create("access");
+    tcase_add_test(tc_access, test_vec2_access);
+    suite_add_tcase(s, tc_access);
+
+    tc_arithm = tcase_create("arithmetic");
+    tcase_add_test(tc_arithm, test_vec2_add);
+    tcase_add_test(tc_arithm, test_vec2_sub);
+    tcase_add_test(tc_arithm, test_vec2_scale);
+    tcase_add_test(tc_arithm, test_vec2_mul_inner);
+    tcase_add_test(tc_arithm, test_vec2_sqr_len);
+    tcase_add_test(tc_arithm, test_vec2_len);
+    tcase_add_test(tc_arithm, test_vec2_norm);
+    tcase_add_test(tc_arithm, test_vec2_min);
+    tcase_add_test(tc_arithm, test_vec2_max);
+    suite_add_tcase(s, tc_arithm);
 
     return s;
 }
