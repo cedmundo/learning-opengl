6405072fc51dbda6bd1c3f7b63218d784e839d4b
Carlos Edmundo Martinez <ced.mendoza@gmail.com>
2017-12-09 02:10:35 -0600

    Missing files from last refactor

---
+++ XX-InfernoKittenEngine/include/ike/hashmap.h
@@ -0,0 +1,153 @@
+#ifndef IKE_SPEC_H
+#define IKE_SPEC_H
+#include <stdio.h>
+
+#define IKE_SPEC_MAP_MISSING (-3) // No such element
+#define IKE_SPEC_MAP_FULL (-2) // Full
+#define IKE_SPEC_MAP_OMEM (-1) // Out Of Memory
+#define IKE_SPEC_MAP_OK (0) // Ok
+
+#ifndef IKE_ANY
+#define IKE_ANY
+/**
+ * \brief Any arbitary type.
+ *
+ * Used to handle abstract types without touching its data at all.
+ */
+typedef void* ikeAny;
+#endif
+
+/**
+ * \brief Iterator callback.
+ *
+ * This callback must return a map status code, if it returns anything
+ * other than IKE_SPEC_MAP_OK the traversal is terminated. Also, it's highly
+ * recommended to not reenter any HashMap functions, or deadlock may arise.
+ */
+typedef int (*ikeHashMapIterator)(ikeAny, ikeAny);
+
+/* We need to keep keys and values */
+typedef struct _ikeHashMapElement {
+	const char* key;
+	int inuse;
+	ikeAny data;
+} ikeHashMapElement;
+
+/**
+ * \brief Forward declaration for ikeHashMap.
+ *
+ * Clients should use only ikeHashMap exported functions to handle data
+ * within a hashmap, which is basically a HashMap, implementation borrowed
+ * (re-written and sighlty modified for code style propouses) from https://github.com/petewarden/c_hashmap.
+ */
+typedef struct _ikeHashMap {
+    int tablesize;
+    int size;
+    ikeHashMapElement *data;
+} ikeHashMap;
+
+/**
+ * \brief Inits an ikeHashMap.
+ * \param hashmap that is going to be inited.
+ */
+int ikeHashMapInit(ikeHashMap* hashmap);
+
+/**
+ * \brief Iterate over items of hashmap.
+ *
+ * Iteratively call iter param with (userdata, itemdata) as argument for each
+ * element data in hashmap.
+ *
+ * \param hashmap map to iterate.
+ * \param iter function to call.
+ * \param userdata first argument of call.
+ * \return
+ */
+int ikeHashMapIterate(ikeHashMap* hashmap, ikeHashMapIterator iter, ikeAny userdata);
+
+/**
+ * \brief Puts a value with hashmapified key.
+ *
+ * \param hashmap where value is going to be stored.
+ * \param key to hash.
+ * \param item to store.
+ * \return IKE_SPEC_MAP_OK or IKE_SPEC_MAP_OMEM.
+ */
+int ikeHashMapPut(ikeHashMap* hashmap, const char *key, ikeAny item);
+
+/**
+ * \brief Gets a value using hashmapified key.
+ *
+ * \param hashmap where value is stored.
+ * \param key to hash.
+ * \param item (output) to copy value from map.
+ * \return IKE_SPEC_MAP_OK or IKE_SPEC_MAP_MISSING.
+ */
+int ikeHashMapGet(ikeHashMap* hashmap, const char *key, ikeAny *item);
+
+/**
+ * \brief Gets an integer using hashmapified key.
+ *
+ * \param hashmap where value is stored.
+ * \param key to hash.
+ * \param item (output) to copy value from map.
+ * \return IKE_SPEC_MAP_OK or IKE_SPEC_MAP_MISSING.
+ */
+int ikeHashMapGetInt(ikeHashMap* hashmap, const char *key, int *item);
+
+/**
+ * \brief Gets a float using hashmapified key.
+ *
+ * \param hashmap where value is stored.
+ * \param key to hash.
+ * \param item (output) to copy value from map.
+ * \return IKE_SPEC_MAP_OK or IKE_SPEC_MAP_MISSING.
+ */
+int ikeHashMapGetFloat(ikeHashMap* hashmap, const char *key, float *item);
+
+/**
+ * \brief Gets a double using hashmapified key.
+ *
+ * \param hashmap where value is stored.
+ * \param key to hash.
+ * \param item (output) to copy value from map.
+ * \return IKE_SPEC_MAP_OK or IKE_SPEC_MAP_MISSING.
+ */
+int ikeHashMapGetDouble(ikeHashMap* hashmap, const char *key, double *item);
+
+/**
+ * \brief Gets a string using hashmapified key.
+ *
+ * \param hashmap where value is stored.
+ * \param key to hash.
+ * \param item (output) to copy value from map.
+ * \return IKE_SPEC_MAP_OK or IKE_SPEC_MAP_MISSING.
+ */
+int ikeHashMapGetString(ikeHashMap* hashmap, const char *key, char **item);
+
+/**
+ * \brief Remove an element from hashmap.
+ *
+ * \param hashmap where value is stored.
+ * \param key to hash.
+ * \return IKE_SPEC_MAP_OK or IKE_SPEC_MAP_MISSING.
+ */
+int ikeHashMapRemove(ikeHashMap* hashmap, const char *key);
+
+/**
+ * \brief Frees the memory of a hashmap.
+ *
+ * \param hashmap to be free.
+ */
+void ikeHashMapFree(ikeHashMap* hashmap);
+
+/**
+ * \brief Get the current size of a hashmap.
+ *
+ * \param hashmap to get size of.
+ * \return count of elements.
+ */
+int ikeHashMapLength(ikeHashMap* hashmap);
+
+
+#endif /* IKE_SPEC_H */
---
+++ XX-InfernoKittenEngine/tests/hashmap_test.c
@@ -0,0 +1,168 @@
+#include <stdlib.h>
+#include <string.h>
+#include <check.h>
+#include <ike/hashmap.h>
+
+#include <stdio.h>
+
+START_TEST(test_hashmapSimple)
+{
+    ikeHashMap hashmap;
+    ikeHashMapInit(&hashmap);
+
+    int *itest = calloc(1, sizeof(int)); *itest = 7;
+    ck_assert_msg(ikeHashMapPut(&hashmap, "int", itest) == IKE_SPEC_MAP_OK, "could not store int");
+
+    float *ftest = calloc(1, sizeof(float)); *ftest = 3.4f;
+    ck_assert_msg(ikeHashMapPut(&hashmap, "float", ftest) == IKE_SPEC_MAP_OK, "could not store float");
+
+    double *dtest = calloc(1, sizeof(double)); *dtest = 3.4;
+    ck_assert_msg(ikeHashMapPut(&hashmap, "double", dtest) == IKE_SPEC_MAP_OK, "could not store double");
+
+    char *stest = calloc(12, sizeof(char)); strcpy(stest, "hello world");
+    ck_assert_msg(ikeHashMapPut(&hashmap, "string", stest) == IKE_SPEC_MAP_OK, "could not store string");
+
+    int *itestr;
+    ck_assert_msg(ikeHashMapGet(&hashmap, "int", (ikeAny *) &itestr) == IKE_SPEC_MAP_OK, "could not read int");
+    ck_assert_msg(*itest == *itestr, "stored integer is not equal than expected");
+
+    float *ftestr;
+    ck_assert_msg(ikeHashMapGet(&hashmap, "float", (ikeAny *) &ftestr) == IKE_SPEC_MAP_OK, "could not read float");
+    ck_assert_msg(*ftest == *ftestr, "stored float is not equal than expected");
+
+    double *dtestr;
+    ck_assert_msg(ikeHashMapGet(&hashmap, "double", (ikeAny *) &dtestr) == IKE_SPEC_MAP_OK, "could not read double");
+    ck_assert_msg(*dtest == *dtestr, "stored double is not equal than expected");
+
+    char *stestr;
+    ck_assert_msg(ikeHashMapGet(&hashmap, "string", (ikeAny *) &stestr) == IKE_SPEC_MAP_OK, "could not read string");
+    ck_assert_msg(strcmp(stest, stestr) == 0, "stored string is not equal than expected");
+
+    free(itest);
+    free(ftest);
+    free(dtest);
+    free(stest);
+
+    ikeHashMapFree(&hashmap);
+}
+END_TEST
+
+START_TEST(test_hashmapGetInt)
+{
+    ikeHashMap hashmap;
+    ikeHashMapInit(&hashmap);
+
+    int *itest = calloc(1, sizeof(int)); *itest = 7;
+    int itestr;
+
+    ck_assert_msg(ikeHashMapPut(&hashmap, "int", itest) == IKE_SPEC_MAP_OK, "could not store int");
+    ck_assert_msg(ikeHashMapGetInt(&hashmap, "int", &itestr) == IKE_SPEC_MAP_OK, "could not read int");
+    ck_assert_msg(*itest == itestr, "stored integer is not equal than expected");
+    free(itest);
+}
+END_TEST
+
+START_TEST(test_hashmapGetFloat)
+{
+    ikeHashMap hashmap;
+    ikeHashMapInit(&hashmap);
+
+    float *ftest = calloc(1, sizeof(float)); *ftest = 5.6;
+    float ftestr;
+
+    ck_assert_msg(ikeHashMapPut(&hashmap, "float", ftest) == IKE_SPEC_MAP_OK, "could not store float");
+    ck_assert_msg(ikeHashMapGetFloat(&hashmap, "float", &ftestr) == IKE_SPEC_MAP_OK, "could not read float");
+    ck_assert_msg(*ftest == ftestr, "stored float is not equal than expected");
+    free(ftest);
+}
+END_TEST
+
+START_TEST(test_hashmapGetDouble)
+{
+    ikeHashMap hashmap;
+    ikeHashMapInit(&hashmap);
+
+    double *dtest = calloc(1, sizeof(double)); *dtest = 5.6;
+    double dtestr;
+
+    ck_assert_msg(ikeHashMapPut(&hashmap, "double", dtest) == IKE_SPEC_MAP_OK, "could not store double");
+    ck_assert_msg(ikeHashMapGetDouble(&hashmap, "double", &dtestr) == IKE_SPEC_MAP_OK, "could not read double");
+    ck_assert_msg(*dtest == dtestr, "stored double is not equal than expected");
+    free(dtest);
+}
+END_TEST
+
+START_TEST(test_hashmapGetString)
+{
+    ikeHashMap hashmap;
+    ikeHashMapInit(&hashmap);
+
+    char *stest = calloc(12, sizeof(char)); strcpy(stest, "hello world");
+    char *stestr;
+
+    ck_assert_msg(ikeHashMapPut(&hashmap, "string", stest) == IKE_SPEC_MAP_OK, "could not store string");
+    ck_assert_msg(ikeHashMapGetString(&hashmap, "string", &stestr) == IKE_SPEC_MAP_OK, "could not read string");
+    ck_assert_msg(strcmp(stest, stestr) == 0, "stored string is not equal than expected");
+    free(stest);
+}
+END_TEST
+
+static int test_hashmapIterateCount = 0;
+static int test_hashmapIterateHelper(ikeAny udata, ikeAny item) {
+    test_hashmapIterateCount ++;
+    return IKE_SPEC_MAP_OK;
+}
+
+START_TEST(test_hashmapIterate)
+{
+    ikeHashMap hashmap;
+    ikeHashMapInit(&hashmap);
+
+    char *stest1 = calloc(12, sizeof(char)); strcpy(stest1, "hello world");
+    char *stest2 = calloc(12, sizeof(char)); strcpy(stest2, "dlrow olleh");
+
+    ck_assert_msg(ikeHashMapPut(&hashmap, "regular", stest1) == IKE_SPEC_MAP_OK, "could not store string (regular)");
+    ck_assert_msg(ikeHashMapPut(&hashmap, "reverse", stest2) == IKE_SPEC_MAP_OK, "could not store string (reverse)");
+
+    ck_assert_msg(ikeHashMapIterate(&hashmap, test_hashmapIterateHelper, NULL) == IKE_SPEC_MAP_OK, "could not iterate map");
+    ck_assert_msg(test_hashmapIterateCount == 2, "unexpected iteration keys count");
+
+    free(stest1);
+    free(stest2);
+}
+END_TEST
+
+Suite *mat4Suite(void)
+{
+    Suite *s;
+    TCase *tc_core;
+
+    s = suite_create("ike/hashmap");
+
+    tc_core = tcase_create("core");
+    tcase_add_test(tc_core, test_hashmapSimple);
+    tcase_add_test(tc_core, test_hashmapGetInt);
+    tcase_add_test(tc_core, test_hashmapGetFloat);
+    tcase_add_test(tc_core, test_hashmapGetDouble);
+    tcase_add_test(tc_core, test_hashmapGetString);
+    tcase_add_test(tc_core, test_hashmapIterate);
+    suite_add_tcase(s, tc_core);
+    return s;
+}
+
+int main(void)
+{
+    int numfailed;
+    Suite *s;
+    SRunner *sr;
+
+    s = mat4Suite();
+    sr = srunner_create(s);
+
+    srunner_run_all(sr, CK_NORMAL);
+    numfailed = srunner_ntests_failed(sr);
+
+    srunner_free(sr);
+    return (numfailed == 0) ? EXIT_SUCCESS : EXIT_FAILURE;
+}
+
